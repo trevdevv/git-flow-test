2b7be1c5d67c630f726d7456e672acfef5f8cd03
trevor <t1perez@ucsd.edu>
2020-05-30 18:12:10 -0700

    test: should pass now

--- .github/workflows/commitlint.config.js
+++ .github/workflows/commitlint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: { 
-    "subject-case": [2, "always", "sentence-case"] 
+    "subject-case": [0, "always", "sentence-case"],
+    "header-case": [0, "always",
+    "lower-case"],
+    "scope-case": [0, "always",
+    "lower-case"],
+    "type-enum": [0, "always",
+    ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
   }
 };
