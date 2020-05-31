9dba94c225806bb836a0d9a848b6bf0245f085b1
trevor <t1perez@ucsd.edu>
2020-05-30 18:36:27 -0700

    test: fix msg
    fix test msg

--- .github/workflows/commitlint.config.js
+++ .github/workflows/commitlint.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: { 
-    "subject-case": [0, "always", "sentence-case"],
+    "subject-case": [0, "never", "sentence-case"],
     "header-case": [0, "always",
     "lower-case"],
     "scope-case": [0, "always",
     "lower-case"],
-    "type-enum": [0, "always",
+    "type-enum": [0, "never",
     ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
     "type-empty": [0, "never"],
     "subject-empty": [0, "never"],
