2c3a4d144d05cca6ef62acc9a7aa9233e3cbdab8
trevor <t1perez@ucsd.edu>
2020-06-01 18:55:32 -0700

    fix: fix eslint issues
    fix eslint issues

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,31 +1,31 @@
 // importing the javascript function
 const mathOps = require('../src/calculator.js')
 
-describe("Calculator tests", () => {
-  test("add 2 numbers", () => {
+describe('Calculator tests', () => {
+  test('add 2 numbers', () => {
     // arrange & act
-    const val = mathOps.sum(3,4)
+    const val = mathOps.sum(3, 4)
 
     // assert
     expect(val).toBe(7)
   })
-  test("add 2 numbers to get success response", () => {
+  test('add 2 numbers to get success response', () => {
     // arrange & act
-    const val = mathOps.sum(3,4)
+    const val = mathOps.sum(3, 4)
 
     // assert
     expect(val).toBe(7)
   })
-  test("divide 2 numbers", () => {
+  test('divide 2 numbers', () => {
     // arrange & act
-    const val = mathOps.divide(6,2)
+    const val = mathOps.divide(6, 2)
 
     // assert
     expect(val).toBe(3)
   })
-  test("multiple 2 numbers", () => {
+  test('multiple 2 numbers', () => {
     // arrange & act
-    const val = mathOps.multiply(6,2)
+    const val = mathOps.multiply(6, 2)
 
     // assert
     expect(val).toBe(12)
